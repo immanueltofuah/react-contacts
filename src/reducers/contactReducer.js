c057675cfefa2c3b3991e3448337ae33ffb82afc
Manuel <tofuah@gmail.com>
2021-10-10 12:19:27 +0100

    created reducer function

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,20 @@
+const initialState= {
+    users:[
+        {name:"winston",
+        email:"winston9@gmail.com",
+        location: "accra"},
+    
+        {name:"gabby",
+        email:"gabby22@gmail.com",
+        location: "tema"},
+    ]
+    
+    }
+    
+    const usersReducer =(state = initialState, action)=>{
+    return state
+    
+    
+    };
+    
+    export default usersReducer;
