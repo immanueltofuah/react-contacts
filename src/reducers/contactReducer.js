c32c510a7284cb3d7a006626068e41b40f5a184e
Manuel <tofuah@gmail.com>
2021-10-17 08:53:35 +0100

    modified return

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -6,8 +6,11 @@ const initialState= {
     }
     
     const contactReducer =(state = initialState, action)=>{
-    return state
-    
+        switch (action.type) {
+            case "ADD_USER":
+        
+        return { ...state, users: [...state.users, action.payload]}
+    }
     
     };
     
