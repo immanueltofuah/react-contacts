e622035767e6eb87ae2d452d7e184f6ca93618ec
Manuel <tofuah@gmail.com>
2021-11-02 17:08:34 +0000

    import firebase, reduxreactfirebase, firestore

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,8 @@
 import {createStore} from "redux";
+import firebase from "../firebase/Config";
+import { getFirebase,reduxReactFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
 import contactReducer from "../reducers/contactReducer";
 
 
