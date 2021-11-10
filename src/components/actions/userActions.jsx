d6cf30c11629f18aa6e17e6c716405cbef11342f
Manuel <tofuah@gmail.com>
2021-11-10 07:39:50 +0000

    added export const getAllUsers

--- src/components/actions/userActions.jsx
+++ src/components/actions/userActions.jsx
@@ -28,3 +28,21 @@ updatedUser ) => {
 
         };
 };
+
+export const getAllUsers = () => {
+	return (dispatch, state, { getFirestore }) => {
+		getFirestore()
+			.collection("users")
+			.onSnapshot(
+				(snapshot) => {
+					let users = [];
+					snapshot.forEach((doc) => {
+						users.push(doc.data());
+					});
+					console.log(users);
+					dispatch({ type: "SET_ALL_USERS", payload: users });
+				},
+				(error) => {}
+			);
+	};
+};
