5c87b4061683b7f7f1f52383eae3f44dc94d6866
Manuel <tofuah@gmail.com>
2021-11-19 16:58:52 +0000

    modified function

--- src/App.js
+++ src/App.js
@@ -4,51 +4,23 @@ import { useState, useEffect} from "react";
 import ContactList from "./components/ContactList";
 import {connect} from "react-redux";
 import {getAllUsers} from "./components/actions/userActions";
-function App(props) {
-  const [users, setUsers] = useState([]);
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter, Route } from "react-router-dom";
+import Register from "./pages/Register";
 
-  
 
-   //function to edit user
-   function editUser(newDetails, userId) {
-    //maps through the users array and updates
-    //the user with the same user id
-    const u = users.map((user) => {
-      if (userId === user.id) {
-        return newDetails;
-      } else {
-        return user;
-      }
-    });
-
-     //updates the users state to contain the edited user
-     setUsers(u);
-    }
-
-    //funtion to delete a user
-  function deleteUser(userId) {
-    //loops through the users state and removes the user with the same id
-    const filteredUsers = users.filter((user) => {
-      return userId !== user.id;
-    });
-
-    //sets the users state to the filtered users array
-    setUsers(filteredUsers);
-  }
-
-  useEffect(()=>{
-    props.getAllUsers();
-  },[]);
-
+function App(){  
   return (
-    <div>
-      <ContactsForm />
-      <ContactList  />
-    </div>
-  );
-}
-const mapDispatchToProps = {
-  getAllUsers,
-}
-
-export default connect(null, mapDispatchToProps)(App);
+   <BrowserRouter>
+ <ProtectedRoute exact path="/" component={Home} />
+ <Route path="/login" component={Login} />
+ <Route path="/register" component={Register} />
+ </BrowserRouter>
+ );
+ }
+  
+   
+ 
+ export default App;
