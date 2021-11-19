24741e30d75541786a3262fb5e6f87ba7cd32f2f
Manuel <tofuah@gmail.com>
2021-11-19 15:17:42 +0000

    import signout, connect usedip,getallusers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React,{useEffect} from "react";
 import ContactForm from "../components/ContactsForm";
 import ContactList from "../components/ContactList";
-
+import { signOut } from "../components/actions/authAction";
+import { useDispatch, connect } from "react-redux";
+import { reactReduxFirebase } from "react-redux-firebase";
+import { getAllUsers } from "../components/actions/userActions";
 
 function Home(props) {
     useEffect(() => {
