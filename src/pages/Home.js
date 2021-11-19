d656e6b676c3e6e1f0d84b253fc52d3adfa55967
Manuel <tofuah@gmail.com>
2021-11-19 15:13:23 +0000

    copied code for home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,24 @@
 import React,{useEffect} from "react";
 import ContactForm from "../components/ContactsForm";
 import ContactList from "../components/ContactList";
+
+
+function Home(props) {
+    useEffect(() => {
+		props.getAllUsers();
+	}, []);
+ 
+  return (
+    <div>
+        <button onClick={() => props.signOut()}>Sign out</button>
+      <UserForm 
+      />
+      <UserList/>
+    </div>
+  );
+}
+const mapDispatchToProps = {
+  getAllUsers: getAllUsers, signOut
+};
+  
+  export default connect(null, mapDispatchToProps)(Home);
